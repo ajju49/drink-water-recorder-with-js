09b9ce85f66ee1340ec125df0a20dabd17ceb7dc
ajju49 <89016384+ajju49@users.noreply.github.com>
2021-10-10 10:25:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.getElementById('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+updateBigCup()
+
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highlightCups(idx))
+})
+
+function highlightCups(idx){
+    if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')){
+        idx--
+    }
+//above condition is , eg. if 3rd cup is full but its next cup (eg.4th cup)
+// is not full then , we click on 3rd cup it becomes empty  
+
+    smallCups.forEach((cup, idx2) => { 
+        //we already use idx  to mention index of cup on which we click 
+        // now idx2 used for index of filled cups
+        if(idx2 <= idx) {
+            //when we click on any cup , the all cups before tht should filled 
+         cup.classList.add('full')
+        }                                                                                                                                                                                                                                                           
+        else{
+            cup.classList.remove('full')
+        }
+    })
+
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length                
+    const totalCups = smallCups.length
+
+    console.log(totalCups)
+
+    if(fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+    }
+
+    
+
+    // else{
+    //     percentage.style.visibility = 'visible'
+    //     percentage.style.height = `${fullCups / totalCups*330}px`
+    //     //to show filled bigcup we take total fullcups and divide them by total cups 
+    //     //and multiply it with 330 (which is height of our bigcup (330px))
+    // }
+}
